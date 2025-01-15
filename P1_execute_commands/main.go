1ccf04e91b611034bb47fb4afff08bbac1616a17
devkaare <kaarehofferdal@gmail.com>
2025-01-15 23:10:44 +0100

    Feat

--- P1_execute_commands/main.go
+++ P1_execute_commands/main.go
@@ -1,3 +1,5 @@
+// NOTE: Don't know why the course creator used snake case, but I'm following along so I will aswell... FOR NOW
+
 package main
 
 import (
@@ -6,17 +8,22 @@ import (
 	"os/exec"
 )
 
-func main() {
-	command := "date"
-
-	// NOTE: Don't know why the course creator used snake case, but I'm following along so I will aswell... FOR NOW
-	cmd_obj := exec.Command(command)
+func executeCommand(command string, args_arr []string) (err error) {
+	args := args_arr
+	cmd_obj := exec.Command(command, args...)
 	cmd_obj.Stdout = os.Stdout
 	cmd_obj.Stderr = os.Stderr
 
-	err := cmd_obj.Run()
+	err = cmd_obj.Run()
 
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	return nil
+}
+
+func main() {
+	command := "ls"
+	executeCommand(command, []string{"-l"})
 }
