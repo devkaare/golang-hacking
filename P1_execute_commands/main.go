0f1976f51ba774a964e4f595c09702223656a7d8
devkaare <kaarehofferdal@gmail.com>
2025-01-15 22:46:51 +0100

    Chore

--- P1_execute_commands/main.go
+++ P1_execute_commands/main.go
@@ -9,6 +9,7 @@ import (
 func main() {
 	command := "date"
 
+	// NOTE: Don't know why the course creator used snake case, but I'm following along so I will aswell... FOR NOW
 	cmd_obj := exec.Command(command)
 	cmd_obj.Stdout = os.Stdout
 	cmd_obj.Stderr = os.Stderr
