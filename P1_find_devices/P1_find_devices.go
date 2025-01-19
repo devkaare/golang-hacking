5d610b3e27842a1b446c102682e6c4e96551910f
devkaare <kaarehofferdal@gmail.com>
2025-01-19 04:10:48 +0100

    Feat

---
+++ P1_find_devices/P1_find_devices.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"github.com/google/gopacket/pcap"
+	"log"
+)
+
+func main() {
+	devices, err := pcap.FindAllDevs()
+	if err != nil {
+		log.Panic(err)
+	}
+	for index, dev := range devices {
+		fmt.Println(index, " ", dev.Name)
+
+		for _, add := range dev.Addresses {
+			fmt.Println("\tIP: ", add.IP)
+			fmt.Println("\tNM: ", add.Netmask)
+		}
+	}
+}
---
+++ P1_find_devices/go.mod
@@ -0,0 +1,5 @@
+module golang-hacking/PacketCapturing
+
+go 1.23.4
+
+require github.com/google/gopacket v1.1.19 // indirect
